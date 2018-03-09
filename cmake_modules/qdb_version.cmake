90cc524286d52f8aa0bd3bdf629b71fb5db6e8e8
Marek Kurdej <marek@quasardb.net>
2018-03-09 11:08:39 +0100

    Bump version to 2.5.0.0-nightly.0.

--- cmake_modules/qdb_version.cmake
+++ cmake_modules/qdb_version.cmake
@@ -2,7 +2,7 @@ if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
     find_package(Git)
 endif()
 
-set(QDB_VERSION_PREFIX "2.4.0") # the default
+set(QDB_VERSION_PREFIX "2.5.0") # the default
 
 string(TIMESTAMP QDB_COPYRIGHT_YEAR "%Y")
 
