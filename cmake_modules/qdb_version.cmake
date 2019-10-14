53e94cada9bc8b11a96ba7481deec11bc83307d6
vianney <vianney.p@outlook.com>
2019-10-14 17:59:33 +0200

    Bump version to 3.4.3.0.

--- cmake_modules/qdb_version.cmake
+++ cmake_modules/qdb_version.cmake
@@ -2,7 +2,7 @@ if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
     find_package(Git)
 endif()
 
-set(QDB_VERSION_PREFIX "3.4.2") # the default
+set(QDB_VERSION_PREFIX "3.4.3") # the default
 
 string(REPLACE "." ";" QDB_VERSION_LIST ${QDB_VERSION_PREFIX})
 
