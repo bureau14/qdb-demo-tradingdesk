f5bf35c483097b6daa1fe2bf278f58ca5b4a0028
terngkub <terngnattapol@gmail.com>
2020-07-07 10:39:39 +0200

    Bump version to 3.8.5.0-nightly.0.

--- cmake_modules/qdb_version.cmake
+++ cmake_modules/qdb_version.cmake
@@ -2,7 +2,7 @@ if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
     find_package(Git)
 endif()
 
-set(QDB_VERSION_PREFIX "3.8.4") # the default
+set(QDB_VERSION_PREFIX "3.8.5") # the default
 
 string(REPLACE "." ";" QDB_VERSION_LIST ${QDB_VERSION_PREFIX})
 
