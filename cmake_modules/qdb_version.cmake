377ac4e785b6873a47add81bfa361d474a448305
terngkub <terngnattapol@gmail.com>
2020-08-03 18:43:12 +0200

    Bump version to 3.11.0.

--- cmake_modules/qdb_version.cmake
+++ cmake_modules/qdb_version.cmake
@@ -2,7 +2,7 @@ if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
     find_package(Git)
 endif()
 
-set(QDB_VERSION_PREFIX "3.9.0") # the default
+set(QDB_VERSION_PREFIX "3.11.0") # the default
 
 string(REPLACE "." ";" QDB_VERSION_LIST ${QDB_VERSION_PREFIX})
 
