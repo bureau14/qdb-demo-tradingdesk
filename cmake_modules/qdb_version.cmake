27e92d840261194290d9d128737727f55879c430
vianney <vianney.p@outlook.com>
2020-09-08 08:40:50 +0200

    Bump version to 3.9.3-nightly.

--- cmake_modules/qdb_version.cmake
+++ cmake_modules/qdb_version.cmake
@@ -2,7 +2,7 @@ if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
     find_package(Git)
 endif()
 
-set(QDB_VERSION_PREFIX "3.9.2") # the default
+set(QDB_VERSION_PREFIX "3.9.3") # the default
 
 string(REPLACE "." ";" QDB_VERSION_LIST ${QDB_VERSION_PREFIX})
 
